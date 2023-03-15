20ead48ea10a361627ce2c6e8d4dd09bc4eb3205
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2023-03-15 17:38:14 -0300

    cambios

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -9,67 +9,23 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
-// const getPokemonUrls = async() => {
-//     let pokemonUrls = [];
-//     let apiUrl = 'https://pokeapi.co/api/v2/pokemon';
-//     while (apiUrl) { //Lo sigue trayendo mientras la propiedad next no sea null.
-//       const response = await axios.get(apiUrl);
-//       const data = response.data;
-//       pokemonUrls.push(...data.results.map(pokemon => pokemon.url)); //pushea individualmente las URLs en lugar de todo el array
-//       apiUrl = data.next; //seteas la variable para que data tome el .next que sigue
-//     }
-//     console.log(pokemonUrls)
-//     return pokemonUrls;
-// }
 
-// // tengo un arreglo con ["https://pokeapi.co/api/v2/pokemon/1/"", "https://pokeapi.co/api/v2/pokemon/2/", "https://pokeapi.co/api/v2/pokemon/3/"]
-
-// async function getAllPokemons() {
-// const pokemoncitos = []
-// const pokemonsHere = await getPokemonUrls()
-// await Promise.all(pokemonsHere.map(async (pok) => { 
-//     const response = await axios.get(pok);
-//     pokemoncitos.push(
-//     { 
-//         name: response.data.name,
-//         hp: response.data.stats[0].base_stat,
-//         attack: response.data.stats[1].base_stat,
-//         defense: response.data.stats[2].base_stat,
-//         speed: response.data.stats[5].base_stat,
-//         height: response.data.height,
-//         weight: response.data.weight,
-//     })
-// }));
-// console.log(pokemoncitos)
-// return pokemoncitos;
-
-// }
-  
-// router.get('/pokemons', async (req, res, next) => {
-//     try {
-//       const pokemonis = await getAllPokemons()
-//       console.log(pokemonis)
-      
-//       if (pokemonis.length === 0) {
-//           res.status(404).send('No se encontraron pokemones.');
-//         } else {
-//         console.log(pokemonis)
-//         res.status(200).send(pokemonis);
-//       }
-//     } catch (error) {
-//       next(error);
-//     }
-//   });
 
 
 const getPokemons = async () => {
     try {
-      const allPokemons = await axios.get("https://pokeapi.co/api/v2/pokemon");
+      let apiUrl = 'https://pokeapi.co/api/v2/pokemon';
+      let pokemons = [];
   
-      const pokemons = await Promise.all(
-        allPokemons.data.results.map(async (p) => {
+      while (apiUrl) {
+        const response = await axios.get(apiUrl);
+        const data = response.data;
+  
+        // Obtener la información de cada pokemon
+        const pokemonsData = await Promise.all(data.results.map(async (p) => {
           const pokemonData = await axios.get(p.url);
           const types = pokemonData.data.types.map((t) => t.type.name);
+  
           return {
             id: pokemonData.data.id,
             name: pokemonData.data.name,
@@ -82,30 +38,36 @@ const getPokemons = async () => {
             weight: pokemonData.data.weight,
             types: types
           };
-        })
-      );
+        }));
+  
+        pokemons.push(...pokemonsData);
+  
+        // Setear la variable apiUrl para seguir trayendo datos
+        apiUrl = data.next;
+      }
+  
       return pokemons;
     } catch (err) {
       console.log(err);
     }
   };
-
+  
   router.get('/pokemons', async (req, res, next) => {
-        try {
-          const pokemonis = await getPokemons()
-          console.log(pokemonis)
-          
-          if (pokemonis.length === 0) {
-              res.status(404).send('No se encontraron pokemones.');
-            } else {
-            console.log(pokemonis)
-            res.status(200).send(pokemonis);
-          }
-        } catch (error) {
-          next(error);
-        }
-      });
-    
+    try {
+      const pokemonis = await getPokemons()
+      console.log(pokemonis)
+      
+      if (pokemonis.length === 0) {
+        res.status(404).send('No se encontraron pokemones.');
+      } else {
+        console.log(pokemonis)
+        res.status(200).send(pokemonis);
+      }
+    } catch (error) {
+      next(error);
+    }
+  });
+  
 
 
 
